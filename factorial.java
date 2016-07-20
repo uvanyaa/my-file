bad68cdd20b903eccbb4b7638976221d9fc78b3b
uvanyaa <uvanyaaksr@gmail.com>
2016-07-20 11:49:19 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,13 @@
+
+public class factorial {
+public static void main(String[] args) {
+	int i,fact=1;
+	int num=5;
+	for(i=1;i<=num;i++)
+	{
+		fact=fact*i;
+	}
+		System.out.println("factoraial of a given number : "+fact);
+		}
+
+}
