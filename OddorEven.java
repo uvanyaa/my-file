69523d44566fb2ade83af278d1420c8049244f78
uvanyaa <uvanyaaksr@gmail.com>
2016-07-20 12:13:36 +0530

    Add files via upload

---
+++ OddorEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class OddorEven {
+public static void main(String[] args) {
+	int a=0;
+	if(a>0)
+	{
+		System.out.println("number is +ve");
+	}else if(a<0)
+	{
+		System.out.println("number is -ve");
+	}else
+	{
+		System.out.println("number is 0");
+	}
+	}
+
+}
